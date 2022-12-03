b4de2e6120ccd0db488579dddc4cf281ff53c565
unknown <maruf0108.1@g,ail.com>
2022-12-03 16:49:45 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   }
+}
